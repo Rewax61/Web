8c54daccb5546972fe68be896ffe391f22303a32
ordi2116328 <ordi2116328@educ.normandie.fr>
2022-10-08 16:46:51 +0200

    Enregistrement

--- code.php
+++ code.php
@@ -2,11 +2,8 @@
     $id = $_POST['id'];
     $password = $_POST['password'];
     echo '<script src="js/script-user.js"></script>';
-    if ($id == 'armand.llz' && $password == 'motdepasse') {
-        echo '<script>defineUser("Armand","Lelaizant")</script>';
-        echo '<script>window.location.replace("accueil.html")</script>';
-    }elseif ($id == 'titi' && $password == 'salut') {
-        echo '<script>defineUser("Tristan","Lelaizant")</script>';
+    if ($id == 'root' && $password == 'passwd') {
+        //echo '<script>defineUser("Armand","Lelaizant")</script>';
         echo '<script>window.location.replace("accueil.html")</script>';
     }else {
         echo '<script>alert("Identifiant ou mot de passe incorrecte"); window.location.replace("/")</script>';        
--- js/script-accueil.js
+++
@@ -1,2 +0,0 @@
-var user = JSON.parse(localStorage.getItem('user'));
-document.getElementById('titre').innerHTML += " " + user.prenom + " " + user.nom;
